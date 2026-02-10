d639a1849a48a05857985f54700fa68db31b66b3
arshad21ibrahim-eng <arshad21ibrahim@gmail.com>
2026-02-10 12:00:28 +0530

    Change CORS configuration to allow all origins
    Updated CORS middleware to allow all origins.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: process.env.FRONTEND_URL || "*" }));
+app.use(cors());
 
 /* -------------------- MongoDB Connection (with retry) -------------------- */
 const connectDB = async (retries = 5) => {
